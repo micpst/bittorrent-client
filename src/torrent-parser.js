04e023f0671fb502b09f64c1300b44910f76b02a
micpst <m.pstrag.kontakt@gmail.com>
2022-03-23 11:28:06 +0100

    Complete torrent parser

--- src/torrent-parser.js
+++ src/torrent-parser.js
@@ -1,6 +1,19 @@
+import crypto from 'crypto';
 import fs from 'fs';
 import bencode from 'bencode';
 
-const open = filepath => bencode.decode(fs.readFileSync(filepath));
+export const open = filepath =>
+    bencode.decode(fs.readFileSync(filepath));
 
-export { open };
+export const size = torrent =>
+    torrent.info.files ?
+        torrent.info.files
+            .map(file => BigInt(file.length))
+            .reduce((a, b) => a + b) :
+        BigInt(torrent.info.length);
+
+export const infoHash = torrent =>
+    crypto
+        .createHash('sha1')
+        .update(bencode.encode(torrent.info))
+        .digest();
