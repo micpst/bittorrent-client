4565e8cb4324e9f31aecc68b3715b76ed66e7e61
micpst <m.pstrag.kontakt@gmail.com>
2022-03-24 12:02:15 +0100

    Fix function generatPeerId

--- src/utility.js
+++ src/utility.js
@@ -3,11 +3,11 @@ import crypto from 'crypto';
 let id = null;
 
 export const generatePeerId = () => {
-    if (id !== null)
-        return id;
-    const newId = Buffer.alloc(20, '-MP0001-');
-    crypto.randomBytes(13).copy(newId, 7);
-    return newId;
+    if (id === null) {
+        id = Buffer.alloc(20, '-MP0001-');
+        crypto.randomBytes(13).copy(id, 7);
+    }
+    return id;
 }
 
 export const chunk = (iterable, chunkSize) =>
